36baeb1be3e07d441bed218958f72a997ad4b07c
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-29 12:16:15 +0530

    Update Dyanamic Array.py

--- Hackerrank/Dynamic Array/Dyanamic Array.py
+++ Hackerrank/Dynamic Array/Dyanamic Array.py
@@ -1,14 +1,14 @@
 if __name__ == "__main__":
-    N, Q = map(int, input().strip().split(' ')) #Taking input
-    sequences = [[] for _ in range(N)] #Creating a 2D array
+    N, Q = map(int, input().strip().split(' ')) 
+    sequences = [[] for _ in range(N)] 
     last_answer = 0 
 
     for q in range(Q):
-        q_type, x, y = map(int, input().strip().split(' ')) #Spliting q_type, x and y
-        seq_num = (x ^ last_answer) % N #Formula given in problem statement
+        q_type, x, y = map(int, input().strip().split(' ')) 
+        seq_num = (x ^ last_answer) % N 
 
         if q_type == 2:
-            last_answer = sequences[seq_num][y % len(sequences[seq_num])] #Updating lastAnswer
-            print(last_answer) #Printing lastAnswer
+            last_answer = sequences[seq_num][y % len(sequences[seq_num])] 
+            print(last_answer) 
         else:
-            sequences[seq_num].append(y) #Appending y
+            sequences[seq_num].append(y) 
