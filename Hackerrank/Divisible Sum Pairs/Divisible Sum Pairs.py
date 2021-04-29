390411a49ae699cd79d5588787979662052ffb8f
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-29 12:15:48 +0530

    Update Divisible Sum Pairs.py

--- Hackerrank/Divisible Sum Pairs/Divisible Sum Pairs.py
+++ Hackerrank/Divisible Sum Pairs/Divisible Sum Pairs.py
@@ -1,10 +1,9 @@
 def divisibleSumPairs(n, k, ar):
-    # Complete this function
-    count = 0 #initialize variable
-    for i in range(n-1): #iterate 
+    count = 0 
+    for i in range(n-1): 
         j = i+1
         while j < n:
-            if ((ar[i] + ar[j]) % k) == 0:  #check if condition satisfied
-                count += 1 #increment count
+            if ((ar[i] + ar[j]) % k) == 0:  
+                count += 1
             j += 1
     return count
