83fd96c601c1b5d4c18d737c7d16c7f9c9e61446
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-29 12:13:45 +0530

    Update Cutting Boards.py

--- Hackerrank/Cutting Boards/Cutting Boards.py
+++ Hackerrank/Cutting Boards/Cutting Boards.py
@@ -1,14 +1,6 @@
-"""
-Logic:
- We want to minimum cost. First we sort both horizontal and vertical cost array and start with largest element form both array, because cost is derived from total cutting edges multiply with horizontal or vertical cost. 
- So, minimum cost has been derived by minimum cost multiply with cutting edge.
 
-"""
-## Cutting Boards  Greedy ###
-# t is the number of test cases
 t=int(input())
 for i in range(t):
-    # Input the row & column
     row,column = list(map(int,input().split()))
     xa= list(map(int,input().split()))
     ya= list(map(int,input().split()))
@@ -22,19 +14,19 @@ for i in range(t):
     cutedge_x=1
     cutedge_y=1
     while pivot_x<row-1 and pivot_y<column-1:
-        if xarr[pivot_x]>yarr[pivot_y]:   #horizontal cost is greater than vertical cost
+        if xarr[pivot_x]>yarr[pivot_y]:   
             cost+=cutedge_y*xarr[pivot_x]
             cutedge_x+=1
             pivot_x+=1
         else:
-            cost+=cutedge_x*yarr[pivot_y]      #vertical cost is greater than horizontal cost
+            cost+=cutedge_x*yarr[pivot_y]      
             cutedge_y+=1
             pivot_y+=1
-    while pivot_x>=row-1 and pivot_y<column-1:   # adding remaining vertical cost
+    while pivot_x>=row-1 and pivot_y<column-1:   
         cost+=cutedge_x*yarr[pivot_y]
         cutedge_y+=1
         pivot_y+=1
-    while pivot_x<row-1 and pivot_y>=column-1:   # adding remaining horizontal cost
+    while pivot_x<row-1 and pivot_y>=column-1:   
         cost+=cutedge_y*xarr[pivot_x]
         cutedge_x+=1
         pivot_x+=1
