aedc4eefa4b7f74939e7670e92350d6ae46d33e8
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-30 11:20:19 +0530

    Update MaximumSumSuchThatNoElementsAreAdjacent.py

--- GeeksForGeeks/Maximum sum such that no two elements are adjacent/MaximumSumSuchThatNoElementsAreAdjacent.py
+++ GeeksForGeeks/Maximum sum such that no two elements are adjacent/MaximumSumSuchThatNoElementsAreAdjacent.py
@@ -1,12 +1,9 @@
- # A program to find sum of maximum elements such that, no two elements are same
 
-# Given array
 arr = [5, 5, 10, 100, 10, 5] 
 
 sumOdd = 0
 sumEven = 0
 
-# Calculating the sum of odd and even indexed elements separately
 for i in arr:
     if sumEven>sumOdd:
         ele = sumEven  
@@ -15,7 +12,6 @@ for i in arr:
     sumOdd = sumEven + i 
     sumEven = ele 
 
-# Printing the maximum sum calculated
 if sumEven>sumOdd:
     print(sumEven)
 else:
