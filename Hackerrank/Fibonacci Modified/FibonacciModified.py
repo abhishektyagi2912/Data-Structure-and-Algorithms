53220c8bea06318153707b6d9ef926a3c87d7bf2
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-04 02:27:47 +0530

    Update FibonacciModified.py

--- Hackerrank/Fibonacci Modified/FibonacciModified.py
+++ Hackerrank/Fibonacci Modified/FibonacciModified.py
@@ -1,28 +1,21 @@
-#!/bin/python
-
 import math
 import os
 import random
 import re
 import sys
 
-# Complete the fibonacciModified function below.
 def fibonacciModified(t1, t2, n):
-    # making a DP array of size n and default value 0
     dp =[0]*(n)
 
-    # we are given values of index 0 & 1
     dp[0]= t1
     dp[1]= t2
 
-    # running loop for n >=2
     for i in range(2, n, 1):
         dp[i]= dp[i-1]**2 + dp[i-2]
     
     return dp[n-1]
 
 
-# driver function    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
