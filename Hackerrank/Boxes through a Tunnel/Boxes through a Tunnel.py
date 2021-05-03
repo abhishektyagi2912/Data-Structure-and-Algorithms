caefdfe6310c36dbe992a89607307a16a3e1afca
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-04 02:25:37 +0530

    Update Boxes through a Tunnel.py

--- Hackerrank/Boxes through a Tunnel/Boxes through a Tunnel.py
+++ Hackerrank/Boxes through a Tunnel/Boxes through a Tunnel.py
@@ -1,9 +1,6 @@
-# A program in python to find whether a box of a given dimension can pass a tunnel of height 41 
-
-
-t= int(input())                             # Getting number of test cases 
+t= int(input())                             
 
 for i in range(t):
-    a = list(map(int, input().split()))     # Getting the array of dimensions
-    if a[2] < 41:                           # Printing the volume if height of the box is less than 41
+    a = list(map(int, input().split()))     
+    if a[2] < 41:                           
         print(a[0]*a[1]*a[2])
