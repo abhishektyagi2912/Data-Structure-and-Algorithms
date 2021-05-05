dad470b0f0ecef2997c8b2bc1d377e91b68d8b4e
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-05 12:06:52 +0530

    Update Max Array Sum.py

--- Hackerrank/Max Array Sum/Max Array Sum.py
+++ Hackerrank/Max Array Sum/Max Array Sum.py
@@ -1,6 +1,5 @@
 """
 Given an array of integers, find the subset of non-adjacent elements with the maximum sum. 
-Calculate the sum of that subset. It is possible that the maximum sum is , the case when all elements are negative.
 
 """
 
