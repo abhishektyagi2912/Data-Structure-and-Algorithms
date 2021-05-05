cf324c917b1bc6097855d15ab3dbd8f509141225
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-05 12:01:48 +0530

    Update Largest Rectangle.py

--- Hackerrank/Largest Rectangle/Largest Rectangle.py
+++ Hackerrank/Largest Rectangle/Largest Rectangle.py
@@ -10,8 +10,6 @@ Your task is to find the largest solid area in which the mall can be constructed
 # For each element(height) of an array,
 # 1. Search the continuous buildings to the left
 #    - if continous building is at the same height or taller, increment the count
-# 2. Seach the continuous buildings to the right
-#    - if continous building is at the same height or taller, increment the count
 # 3. Calculate the current rectangle area: 
 #    - height of the current building * current count
 # 4. Check if the current area is larger than the current maximum area
