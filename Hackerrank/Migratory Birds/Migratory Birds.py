be4b2d1c15985ce3a09ada12b4befb98a4249d76
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-05 12:12:29 +0530

    Update Migratory Birds.py

--- Hackerrank/Migratory Birds/Migratory Birds.py
+++ Hackerrank/Migratory Birds/Migratory Birds.py
@@ -5,15 +5,12 @@ def migratoryBirds(arr):
     m=[]
     c = dict(Counter(arr))
     for i in c.items():
-        #when 2 or more values are common 
         if i[1]==max(c.values()):
             m.append(i[0])
-     #retrieve smallest id number 
     return min(m)
 
 
 if __name__ == '__main__':
-    #Input List 
     arr_count = int(input().strip())
     arr = list(map(int, input().split(' ')))
     result = migratoryBirds(arr)
