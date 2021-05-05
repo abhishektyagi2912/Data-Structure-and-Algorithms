b2e17f9e6555a4f42229bfe99c376784cd8c98a2
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-05 12:11:58 +0530

    Update mergeTheTools.py

--- Hackerrank/Merge The Tools/mergeTheTools.py
+++ Hackerrank/Merge The Tools/mergeTheTools.py
@@ -8,11 +8,9 @@ def merge_the_tools(string, k):
     for i in merged_list:
         print(remove(i))
 
-# Function to have only unique elements
 def remove(arr):
     return ''.join(dict.fromkeys(arr))
 
-# Internal HackerRank code (already given)
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
