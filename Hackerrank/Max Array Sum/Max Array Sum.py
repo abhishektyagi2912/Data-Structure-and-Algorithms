318bea3a6713dee0828022ef32b7e2f23ba2152e
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-05 12:07:44 +0530

    Update Max Array Sum.py

--- Hackerrank/Max Array Sum/Max Array Sum.py
+++ Hackerrank/Max Array Sum/Max Array Sum.py
@@ -6,8 +6,6 @@ def maxSubsetSum(arr):
     dp[1] = max(arr[1], dp[0]) 
     
     for i in range(2,n):
-       # Therefore we can either take one and then skip one, or skip one and run the subroutine.
         dp[i] = max(arr[i], dp[i-1], arr[i] + dp[i-2])
-    # in the dp, we store the max sum for the subarray up till the length of the subarray.
     #  Hence simply return the last item in this to get the answer
     return dp[-1]
