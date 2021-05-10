641e250086062abc9d2aa33e2c60bd4c849b040c
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-11 01:24:01 +0530

    Update Red Knight's Shortest Path.py

--- Hackerrank/Red Knight's Shortest Path/Red Knight's Shortest Path.py
+++ Hackerrank/Red Knight's Shortest Path/Red Knight's Shortest Path.py
@@ -1,6 +1,4 @@
-# Complete the printShortestPath function below.
 def printShortestPath(n, i_start, j_start, i_end, j_end):
-    # Print the distance along with the sequence of moves.
 
     
     # UL, UR, R, LR, LL, L
