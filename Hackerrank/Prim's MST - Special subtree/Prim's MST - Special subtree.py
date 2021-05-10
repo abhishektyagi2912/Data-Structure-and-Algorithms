a4df6bfeae3701b8c77ed8b73924aa246ab1cfb3
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-11 01:16:05 +0530

    Update Prim's MST - Special subtree.py

--- Hackerrank/Prim's MST - Special subtree/Prim's MST - Special subtree.py
+++ Hackerrank/Prim's MST - Special subtree/Prim's MST - Special subtree.py
@@ -10,7 +10,7 @@ for _ in range(m):
     x, y, r = map(int, input().split())
     conn[x][y] = conn[y][x] = min(r, conn[x].get(y, 200000))
 res = dist[int(input())] = 0
-for _ in range(n):                               # Calculating result
+for _ in range(n):                              
     nodes.sort(key=dist.__getitem__, reverse=True)
     curr = nodes.pop()
     outside[curr] = False
@@ -18,5 +18,5 @@ for _ in range(n):                               # Calculating result
     for x, d in conn[curr].items():
         if outside[x] and dist[x] > d:
             dist[x] = d          
-print(res)                                       # Printing result
+print(res)                                      
 
