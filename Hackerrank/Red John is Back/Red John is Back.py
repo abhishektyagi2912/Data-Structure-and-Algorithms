ffab33d6e1ca9619585952fc29f3d080f6ab8df0
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-11 01:23:31 +0530

    Update Red John is Back.py

--- Hackerrank/Red John is Back/Red John is Back.py
+++ Hackerrank/Red John is Back/Red John is Back.py
@@ -4,7 +4,6 @@ def redJohn(n):
         combos += (math.factorial(n - 3 * (i+1)) / (math.factorial(n - 4 * (i+1)) * math.factorial(i+1)))
     for j in range(2, int(combos + 1)):
         prime = True
-        # if j % 2 gets 0 then it is not a prime
         if j != 2 and j % 2 == 0:
             prime = False
         else:
